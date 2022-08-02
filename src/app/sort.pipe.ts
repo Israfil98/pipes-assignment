0288403124632e05cf3fa01758d76fe8e6c4f050
Israfil98 <israfil.guseynov.98@inbox.ru>
2022-08-02 15:27:05 +0400

    second task done

---
+++ src/app/sort.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+  name: "sort",
+  pure: false,
+})
+export class SortPipe implements PipeTransform {
+  transform(value: any): any {
+    return value.sort((a: any, b: any) => (a["name"] > b["name"] ? 1 : -1));
+  }
+}
